92c280de4f1a6de1f2c6cfed3bcd54c6942dfef1
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2025-12-02 13:11:03 +0700

    first commit

--- js/app.js
+++ js/app.js
@@ -9,43 +9,4 @@ new Vue({
         stokList: [],
         trackingList: [],
     },
-    created() {
-        API.loadData().then((data) => {
-            this.upbjjList = data.upbjjList;
-            this.kategoriList = data.kategoriList;
-            this.pengirimanList = data.pengirimanList;
-            this.paketList = data.paket;
-            this.stokList = data.stok;
-
-            this.trackingList = [];
-
-            if (data.tracking) {
-                data.tracking.forEach((obj) => {
-                    this.trackingList.push(obj);
-                });
-            }
-        });
-    },
-
-    methods: {
-        findTracking(doNumber) {
-            return this.trackingList.find((t) => t.do === doNumber) || null;
-        },
-        nextDO() {
-            const list = this.trackingList;
-
-            if (list.length === 0) {
-                return "DO" + new Date().getFullYear() + "-0001";
-            }
-
-            const lastObj = list[list.length - 1];
-            const lastDO = Object.keys(lastObj)[0];
-
-            const match = lastDO.match(/DO(\d{4})-(\d{4})/);
-
-            const next = parseInt(match[2]) + 1;
-
-            return "DO" + match[1] + "-" + String(next).padStart(4, "0");
-        },
-    },
 });
--- js/components/status-badge.js
+++ js/components/status-badge.js
@@ -1,18 +1,3 @@
 Vue.component("status-badge", {
     template: "#tpl-status-badge",
-    props: ["qty", "safety"],
-    computed: {
-        badgeClass() {
-            if (this.qty === 0) return "badge kosong";
-            if (this.qty < this.safety) return "badge menipis";
-
-            return "badge aman";
-        },
-
-        text() {
-            if (this.qty === 0) return "Kosong";
-            if (this.qty < this.safety) return "Menipis";
-            return "Aman";
-        },
-    },
 });
